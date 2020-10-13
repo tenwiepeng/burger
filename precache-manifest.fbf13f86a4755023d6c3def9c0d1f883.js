a29d286018e015d41ae257368d692765d88b09cc
Tenwie Peng <tenwie.peng@gmail.com>
2020-10-13 12:22:00 -0700

    Updates

---
+++ precache-manifest.fbf13f86a4755023d6c3def9c0d1f883.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac855438d81d57fc67e3423db3262a8",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "bbe41e7cf97e95d6d0ec",
+    "url": "/burger/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "fa359455e8462c1fc7e1",
+    "url": "/burger/static/css/3.97f4e5c4.chunk.css"
+  },
+  {
+    "revision": "8d7cfe4a1684f16b296b",
+    "url": "/burger/static/css/4.efff8329.chunk.css"
+  },
+  {
+    "revision": "d96fb8d3d39cfef8a57d",
+    "url": "/burger/static/css/5.99a5c08a.chunk.css"
+  },
+  {
+    "revision": "2513fdb0742e98f60c8c",
+    "url": "/burger/static/css/main.f19cfc0e.chunk.css"
+  },
+  {
+    "revision": "bbe41e7cf97e95d6d0ec",
+    "url": "/burger/static/js/2.dbf0621f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/burger/static/js/2.dbf0621f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa359455e8462c1fc7e1",
+    "url": "/burger/static/js/3.ac0a9746.chunk.js"
+  },
+  {
+    "revision": "8d7cfe4a1684f16b296b",
+    "url": "/burger/static/js/4.b6aeee73.chunk.js"
+  },
+  {
+    "revision": "d96fb8d3d39cfef8a57d",
+    "url": "/burger/static/js/5.4edf0a20.chunk.js"
+  },
+  {
+    "revision": "2513fdb0742e98f60c8c",
+    "url": "/burger/static/js/main.4e717a2e.chunk.js"
+  },
+  {
+    "revision": "c728f83d2b195155a4f6",
+    "url": "/burger/static/js/runtime-main.1ded6f4d.js"
+  }
+]);
